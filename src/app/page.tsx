f43a5cb2be99f13ec961947931a175d01dbd33e5
soe-un <runterf@gmail.com>
2024-12-10 23:56:32 +0900

    data loading process 고민중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Label from "@/components/label";
 import common from "../../public/locales/ko/common.json";
 import { LABELS, TLABEL, TLINES } from "@/types/type";
@@ -6,20 +7,54 @@ import Day from "@/components/day";
 import { dates } from "@/data";
 import { Hahmlet } from "next/font/google";
 import clsx from "clsx";
+import { useEffect, useState } from "react";
 
 const hahmlet = Hahmlet({ subsets: ["latin"] });
 
 export default function Home() {
   const ko = common;
+  const [isLoading, setIsLoading] = useState(true);
+  const [childrens, setChildrens] = useState<React.ReactElement>();
 
-  // const fetchDateData = async ({ date }: { date: string }) => {
-  //   const res = await fetch(`/api/${date}`);
-  //   const data = await res.json();
-  //   console.log(fetchDateData);
-  //   return data;
-  // };
+  const getData = () => {
+    dates.map(async (d) => {
+      await import(`../data/${d}`)
+        .then((res) => {
+          return (
+            <Day
+              key={d}
+              date={d}
+              childrens={res.default.map((data: TLINES) => {
+                return (
+                  <Lines
+                    key={data.time + data.value}
+                    type={data.type}
+                    time={data.time}
+                    people={data.people}
+                    value={data.value}
+                  />
+                );
+              })}
+            />
+          );
+        })
+        .then((res) => {
+          setChildrens((prev) => (
+            <>
+              {prev}
+              {res}
+            </>
+          ));
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    });
+  };
 
-  // fetchDateData({ date: "241208" });
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <>
@@ -43,28 +78,7 @@ export default function Home() {
           <Label type={LABELS.WHITE as TLABEL} />
         </div>
         <div className="grid gird-cols-1 w-full">
-          {dates.map(async (d) => {
-            const data = await import(`../data/${d}`).then((res) => {
-              return (
-                <Day
-                  key={d}
-                  date={d}
-                  childrens={res.default.map((data: TLINES) => {
-                    return (
-                      <Lines
-                        key={data.time + data.value}
-                        type={data.type}
-                        time={data.time}
-                        people={data.people}
-                        value={data.value}
-                      />
-                    );
-                  })}
-                />
-              );
-            });
-            return data;
-          })}
+          {isLoading ? "Loading ..." : childrens}
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
